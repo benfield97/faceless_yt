b4b518ba3650b4d9136e5d3c1348464f12cda315
Ben Field <fieldben97@gmail.com>
2023-07-11 12:58:14 +0800

    progress on video editor

--- makevid.py
+++ makevid.py
@@ -1,40 +1,50 @@
 import requests
+from dotenv import load_dotenv
+import os
+import glob
+from pydub import AudioSegment
+load_dotenv()
+creatomate_api = os.getenv('creatomate_api')
+root_dir = 'data/audio'
 
-source = {
- 'output_format': 'mp4',
- 'width': 1920,
- 'height': 1080,
- 'elements': [
-  {
-   'type': 'video',
-   'track': 1,
-   'source': 'https://cdn.creatomate.com/demo/drone.mp4'
-  },
-  {
-   'type': 'video',
-   'track': 1,
-   'source': 'https://cdn.creatomate.com/demo/river.mp4',
-   'animations': [
-    {
-     'time': 'start',
-     'duration': 1,
-     'transition': True,
-     'type': 'fade'
+def get_duration(file_path):
+    audio = AudioSegment.from_file(file_path)
+    return len(audio) / 1000.0  # pydub returns length in milliseconds
+
+def makevid(username,points, time, duration, body):
+
+    options = {
+    # The ID of the template that you created in the template editor
+    'template_id': '823ad2be-9b2e-46af-bdf5-f39abf90ed96',
+    
+
+    # Modifications that you want to apply to the template
+    'modifications': {
+        'Body Text': "Hi! 👋 Thanks for trying out Creatomate!",
+        'username': "✨🦖",
+        'points': '👍 100',
+        'time': '4 years ago',
+        'duration': '20 s'
+    },
     }
-   ]
-  }
- ]
-}
 
-response = requests.post(
- 'https://api.creatomate.com/v1/renders',
- headers={
-  # Find your API key under 'Project Settings' in your account:
-  # https://creatomate.com/docs/api/rest-api/authentication
-  'Authorization': 'Bearer Your-API-Key',
-  'Content-Type': 'application/json',
- },
- json={'source': source}
-)
 
-print(response.json())
+    response = requests.post(
+    'https://api.creatomate.com/v1/renders',
+            headers={
+                'Authorization': f'Bearer {creatomate_api}',
+                'Content-Type': 'application/json',
+            },
+            json=options
+            )
+
+    print(response.content)
+
+for dir_name, _, file_list in os.walk(root_dir):
+    print(dir_name)
+    for file_name in file_list:
+        if 'comment' in file_name:
+            full_path = os.path.join(dir_name, file_name)
+            duration = get_duration(full_path)
+            print(f'Duration of {file_name}: {duration} seconds')
+            # Perform further processing here
