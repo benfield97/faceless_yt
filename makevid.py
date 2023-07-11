8509989db2d58e8470f7dbcf28c8222e606aa65e
Ben Field <fieldben97@gmail.com>
2023-07-11 14:50:13 +0800

    improved vid maker

--- makevid.py
+++ makevid.py
@@ -11,7 +11,15 @@ def get_duration(file_path):
     audio = AudioSegment.from_file(file_path)
     return len(audio) / 1000.0  # pydub returns length in milliseconds
 
-def makevid(username,points, time, duration, body):
+def convert_wav_to_mp3(wav_file, mp3_file):
+    # Check if the output file already exists
+    if not os.path.isfile(mp3_file):
+        audio = AudioSegment.from_wav(wav_file)
+        audio.export(mp3_file, format="mp3")
+    return mp3_file
+
+
+def makevid(username,points, time, duration, body, voiceover):
 
     options = {
     # The ID of the template that you created in the template editor
@@ -24,7 +32,8 @@ def makevid(username,points, time, duration, body):
         'username': "✨🦖",
         'points': '👍 100',
         'time': '4 years ago',
-        'duration': '20 s'
+        'duration': duration,
+        'voiceover': voiceover
     },
     }
 
@@ -40,11 +49,27 @@ def makevid(username,points, time, duration, body):
 
     print(response.content)
 
-for dir_name, _, file_list in os.walk(root_dir):
-    print(dir_name)
-    for file_name in file_list:
-        if 'comment' in file_name:
-            full_path = os.path.join(dir_name, file_name)
-            duration = get_duration(full_path)
-            print(f'Duration of {file_name}: {duration} seconds')
-            # Perform further processing here
+
+def process_files(root_dir):
+    result_dict = {}
+    for dir_name, _, file_list in os.walk(root_dir):
+        for file_name in file_list:
+            if 'comment' in file_name:
+                full_path = os.path.join(dir_name, file_name)
+                audio = convert_wav_to_mp3(full_path, full_path.replace('.wav', '.mp3'))
+                duration = get_duration(full_path)
+                print(f'Duration of {file_name}: {duration} seconds')
+                
+                # Create a dictionary for this directory if it doesn't exist
+                if dir_name not in result_dict:
+                    result_dict[dir_name] = {}
+
+                # Save the audio and duration for this file
+                result_dict[dir_name][file_name] = [audio, duration]
+
+    return result_dict
+
+
+print(process_files(root_dir))
+
+#makevid('username', 'points', 'time', duration, 'body', audio)
